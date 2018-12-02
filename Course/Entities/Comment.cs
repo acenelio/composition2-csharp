30eb59ce4fb8b101e343ebbf6ef596d004d1008f
Nelio Alves <acenelio@gmail.com>
2018-12-02 09:29:44 -0200

    Initial implementation

---
+++ Course/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace Course.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Comment c1 = new Comment("Have a nice trip!");
+            Comment c2 = new Comment("Wow that's awesome!");
+            Post p1 = new Post(
+                    DateTime.Parse("21/06/2018 13:05:44"),
+                    "Traveling to New Zealand",
+                    "I'm going to visit this wonderful country!",
+                    12);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Good night");
+            Comment c4 = new Comment("May the Force be with you");
+            Post p2 = new Post(
+                    DateTime.Parse("28/07/2018 23:14:19"),
+                    "Good night guys",
+                    "See you tomorrow",
+                    5);
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+    }
+}
